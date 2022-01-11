924008a1f84475d9f17652109f6a2cf6335a2e8a
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-10 20:44:26 -0500

    rewrote code for prompts to fix prompted order in terminal

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const console = require('console');
 
 // inquirer prompt for initial command-line user input with info for HTML 
-const teamMembers = () => {
- return inquirer.prompt ([
+const main = () => {
+  inquirer
+  .prompt ([
 {
     type: 'input',
     message: 'Enter your team manager`s name.',
@@ -26,71 +27,100 @@ const teamMembers = () => {
     message: 'Enter your office number.',
     name: 'office',
 },
+])  
+.then((data) => {
+    let manager = new Manager(data.teamManager, data.employeeID, data.email, data.office);
+    push(manager);
+    addTeammate();  
+});
+};
+
+// prompt asks if team members are going to be added
+const addTeammate = () => {
+    inquirer
+    .prompt ([
 {
     type: 'list',
     message: 'Select which you would like to add and/or complete next.',
     name: 'addTeam',
     choices: ['Engineer', 'Intern', 'Finish Building My Team']
-    }
+},
 ])
-    .then(({ addTeam }) => {
-    if (addTeam === 'Engineer') {
-        inquirer
-        .prompt ([
-        {
-            type: 'input',
-            message: 'Enter Engineer`s name.',
-            name: 'engineer',
-        },
-        {
-            type: 'input',
-            message: 'Enter employee ID.',
-            name: 'engineerID',
-        },
-        {
-            type: 'input',
-            message: 'Enter email address.',
-            name: 'engineerEmail',
-        },
-        {
-            type: 'input',
-            message: 'Enter GitHub username.',
-            name: 'engineerGithub',
-        },
-        ]);
-        return 
-    } else if (addTeam === 'Intern') {
-        inquirer
-        .prompt ([
-        {
-            type: 'input',
-            message: 'Enter Intern`s name.',
-            name: 'intern',
-        },
-        {   type: 'input',
-            message: 'Enter employee ID.',
-            name: 'interID',
-
-        },
-        {
-            type: 'input',
-            message: 'Enter email address.',
-            name: 'internEmail',
-        },
-        {
-            type: 'input',
-            message: 'Enter Intern`s school name.',
-            name: 'school',
-        }    
-        ]);
-        return
-    } else if (addTeam === 'Finish Building My Team') {
-        console.log('HTML document was created! Check dist/ folder.');
-    }
-})
-
-    
+    .then((data) => {
+        if(data.addTeam === 'Engineer') {
+            addEngineer();
+        } else if (data.addTeam === 'Intern') {
+            addIntern();
+        } else {
+            console.log('HTML document was created! Check dist/ folder.');
+        }
+    })
 };
 
-// funcation call 
-teamMembers();
+// prompt creates engineer in HTML
+const addEngineer = () => {
+    inquirer
+    .prompt ([
+{
+    type: 'input',
+    message: 'Enter Engineer`s name.',
+    name: 'engineer',
+},
+{
+    type: 'input',
+    message: 'Enter employee ID.',
+    name: 'engineerID',
+},
+{
+    type: 'input',
+    message: 'Enter email address.',
+    name: 'engineerEmail',
+},
+{
+    type: 'input',
+    message: 'Enter GitHub username.',
+    name: 'engineerGithub',
+},
+])
+.then((data) => {
+    let engineer = new Engineer(data.engineer, data.engineerID, data.engineerEmail, data.engineerGithub);
+    push(engineer);
+    addTeammate();
+})
+};
+
+// prompt creates intern in HTML
+const addIntern = () => {
+    inquirer
+    .prompt ([
+{
+    type: 'input',
+    message: 'Enter Intern`s name.',
+    name: 'intern',
+},
+{   type: 'input',
+    message: 'Enter employee ID.',
+    name: 'interID',
+
+},
+{
+    type: 'input',
+    message: 'Enter email address.',
+    name: 'internEmail',
+},
+{
+    type: 'input',
+    message: 'Enter Intern`s school name.',
+    name: 'school',
+}    
+])
+.then((data) => {
+    let intern = new Intern(data.intern, data.internID, data.internEmail, data.school);
+    push(intern);
+    addTeammate();
+})
+};
+
+
+// main funcation call 
+main();
